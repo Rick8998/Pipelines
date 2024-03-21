4cad6d380d6b972970b74caa07e1a69ac438b0ed
Rick <riccardo.cecci.rc.rc@gmail.com>
2024-03-21 08:42:46 +0100

    minor changes

--- scripts/writeOnFile.py
+++ scripts/writeOnFile.py
@@ -3,12 +3,16 @@ import sys
 from datetime import datetime
 
 def write_on_file(text, fileName):
+    print("TEST 1")
     time_and_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"{time_and_date}: {text}"
     print(formatted_text)
-    with open(fileName, 'w') as file:
+    try:
+        with open(fileName, 'w') as file:
             file.write(formatted_text + '\n')
-
+    except Exception:
+        with open(fileName, 'w') as file:
+            file.write(formatted_text + '\n')
 
 testo_da_scrivere = "Test"
 path_to_file = "test.txt"
