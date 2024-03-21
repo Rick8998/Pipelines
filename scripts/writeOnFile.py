1d8616c0015aa8146b701845236cb7997af99df4
Rick <riccardo.cecci.rc.rc@gmail.com>
2024-03-21 08:11:17 +0100

    Update writeOnFile.py

--- scripts/writeOnFile.py
+++ scripts/writeOnFile.py
@@ -7,8 +7,8 @@ def write_on_file(text, fileName):
     time_and_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"{time_and_date}: {text}"
     print(formatted_text)
-    with open(fileName, 'w') as file:
-            file.write(formatted_text + '\n')
+    #with open(fileName, 'w') as file:
+    #        file.write(formatted_text + '\n')
 
 
 testo_da_scrivere = "Test"
