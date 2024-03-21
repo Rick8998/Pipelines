44620900901cdd3cf6e66b82f93abe7601a610ab
Rick <riccardo.cecci.rc.rc@gmail.com>
2024-03-21 08:08:44 +0100

    exception handling

--- scripts/writeOnFile.py
+++ scripts/writeOnFile.py
@@ -10,9 +10,11 @@ def write_on_file(text, fileName):
     try:
         with open(fileName, 'w') as file:
             file.write(formatted_text + '\n')
-    except Exception:
-        with open(fileName, 'w') as file:
-            file.write(formatted_text + '\n')
+    except FileNotFoundError:
+        print(f"File '{fileName}' non trovato.")
+    except PermissionError:
+        print(f"Permesso negato per scrivere su '{fileName}'. Assicurati di avere i permessi necessari.")
+
 
 testo_da_scrivere = "Test"
 path_to_file = "test.txt"
