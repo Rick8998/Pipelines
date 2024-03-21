066b5243b91e830d33d2f2a560c102e6998e0a8d
Rick <riccardo.cecci.rc.rc@gmail.com>
2024-03-21 08:10:14 +0100

    Update writeOnFile.py

--- scripts/writeOnFile.py
+++ scripts/writeOnFile.py
@@ -7,13 +7,8 @@ def write_on_file(text, fileName):
     time_and_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"{time_and_date}: {text}"
     print(formatted_text)
-    try:
-        with open(fileName, 'w') as file:
+    with open(fileName, 'w') as file:
             file.write(formatted_text + '\n')
-    except FileNotFoundError:
-        print(f"File '{fileName}' non trovato.")
-    except PermissionError:
-        print(f"Permesso negato per scrivere su '{fileName}'. Assicurati di avere i permessi necessari.")
 
 
 testo_da_scrivere = "Test"
