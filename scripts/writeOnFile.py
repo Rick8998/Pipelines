e27613d994efdb16dfad88e82270cf846ae6ebe7
Rick <riccardo.cecci.rc.rc@gmail.com>
2024-03-21 08:55:10 +0100

    update py and exe

--- scripts/writeOnFile.py
+++ scripts/writeOnFile.py
@@ -7,12 +7,9 @@ def write_on_file(text, fileName):
     time_and_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"{time_and_date}: {text}"
     print(formatted_text)
-    try:
-        with open(fileName, 'w') as file:
+    with open(fileName, 'w') as file:
             file.write(formatted_text + '\n')
-    except Exception:
-        with open(fileName, 'w') as file:
-            file.write(formatted_text + '\n')
+   
 
 testo_da_scrivere = "Test"
 path_to_file = "test.txt"
